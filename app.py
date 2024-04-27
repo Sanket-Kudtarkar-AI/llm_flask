54e0f66bf57d2f9a2c02492fd751aa0ed9431a5a
Sanket <skudtarkar16@gmail.com>
2024-04-27 18:19:45 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from llama_cpp import Llama
+import re
+import tiktoken
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+def num_tokens_from_string(input_text):
+    encoding_name = "cl100k_base"
+
+    encoding = tiktoken.get_encoding(encoding_name)
+    num_tokens = len(encoding.encode(input_text))
+    return num_tokens
+
+
+llm = Llama(model_path="openchat-3.5-1210.Q5_K_M.gguf",
+            n_gpu_layers=50,
+            n_ctx=5000)
+
+
+@app.route('/get_llm_response')
+def get_llm_response():
+    request_data = request.get_json()
+    prompt = request_data["prompt"]
+
+    prompt_ = f'''GPT4 Correct User: {prompt}<|end_of_turn|>GPT4 Correct Assistant:'''
+
+    output = llm(prompt_,
+                 max_tokens=5,
+                 echo=True,
+                 temperature=0.0,
+                 )
+
+    answer = output['choices'][0]['text'].replace(prompt_, "").strip()
+
+    response_data = {"llm_response": answer}
+
+    res = jsonify(response_data)
+    return res
+
+
+if __name__ == '__main__':
+    app.run(debug=False, port=5000)
